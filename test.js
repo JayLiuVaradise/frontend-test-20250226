515e569e43cd53dfa9c3eb8918639de994cc2c5c
JayLiuVaradise <jayliu@varadise.cloud>
2025-02-27 09:27:59 +0800

    core: added a debounce function

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
-const fn=()=>{
+const fn = () => {
     console.log('this is branch feature/20250227');
-    console.log('This line added by new-test-branch')
+    console.log('This line added by new-test-branch');
+
+
 }
 
 fn();
+
+
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...arg) => {
+        if (timeoutId !== null) return;
+        timeoutId = setTimeout(() => {
+            callback(arg);
+        }, wait);
+    }
+}
