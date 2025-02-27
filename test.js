55b071f073de84100f5be33cb6b14223f659805e
JayLiuVaradise <jayliu@varadise.cloud>
2025-02-27 09:22:50 +0800

    core: Added one line in test js file

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const fn=()=>{
-    console.log('this is branch feature/20250227')
+    console.log('this is branch feature/20250227');
+    console.log('This line added by new-test-branch')
 }
 
 fn();
